0c7f6ad676e7d46e06940cc85532134895325e50
dauennnn <chiopyo@naver.com>
2022-11-29 15:01:16 +0900

    first commit

---
+++ kiosk/src/kiosk/Kiosk_main.java
@@ -0,0 +1,12 @@
+package kiosk;
+
+public class Kiosk_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Kiosk_Screen1 m = new Kiosk_Screen1();
+		
+		
+	}
+
+}
